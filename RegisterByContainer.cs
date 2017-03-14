379cd5ea3e6f5bab4fa771fe4be5fc208ba5d6ff
Vesper Lynd <vesperisking@gmail.com>
2017-03-14 15:48:04 +0200

    take2

---
+++ RegisterByContainer.cs
@@ -0,0 +1,19 @@
+﻿namespace TeamInternational
+{
+    using StructureMap;
+    using TeamInternational.Hierarchy;
+
+    public class RegisterByContainer
+    {
+        public RegisterByContainer()
+        {
+            Container = new Container(x =>
+            {
+                x.For<IEngine>().Use<Engine>();
+            });
+        }
+
+
+        public IContainer Container { get; set; }
+    }
+}
